9c9f5dfac91b55965fcfec142f64a56919307385
Rhibro <rhibron@gmail.com>
2025-02-10 09:14:02 +0100

    styles fixes

--- src/components/portfolio/PortData.jsx
+++ src/components/portfolio/PortData.jsx
@@ -4,16 +4,16 @@ export const PortData = [
     {
         pageLink: '',
         image: tripPlanner,
-        projectName: 'trip-planner',
+        projectName: 'trip-planner-routes',
         projectInfo: 
             'This project uses React, JavaScript, HTML, and CSS to build a simple application where you can plan a trip with a form. It also uses an API to show to users weather in their location',
     },
     {
         pageLink: '',
         image: tripPlanner,
-        projectName: 'project-name',
+        projectName: 'movie-selector',
         projectInfo:
-            'Blah blah blah blah...',
+            '"The Movie Selector" is a dynamic web application that allows users to explore movies, search by title, filter by genre, and view detailed information about each movie. The project is powered by The Movie Database (TMDb) API and emphasizes asynchronous operations, user experience, and responsive design.',
     },
     {
         pageLink: '',
