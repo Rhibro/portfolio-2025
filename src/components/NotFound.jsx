f6b2adfbb33818ab8992b935c9c08baedea1115f
Rhibro <rhibron@gmail.com>
2025-03-05 10:26:28 +0100

    styled not found

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -1,13 +1,14 @@
-import Header from "./header/Header";
+// import Header from "./header/Header";
 import { Link } from "react-router-dom";
 
 export default function NotFound() {
     return (
         <>
-        <Header />
+        
         <h1>404 Not Found</h1>
+        <h3>¯\_(ツ)_/¯</h3>
         <h3>Sorry, this page doesn't seem to be available.</h3>
-        <h3>Let's head <Link style={{color: 'blue'}} className="liLink" to="/">Home</Link></h3>
+        <h3 style={{fontWeight: '600'}}>Let's head <Link style={{color: 'blue', textDecoration: 'underline', fontWeight: '600'}} className="liLink" to="/">Home</Link></h3>
         </>
     )
 }
--- src/router/router.jsx
+++ src/router/router.jsx
@@ -29,7 +29,7 @@ const router = createBrowserRouter([
         element: <Contact />,
     },
     {
-        path: '/notfound',
+        path: '*',
         element: <NotFound />,
     },
 ]);
